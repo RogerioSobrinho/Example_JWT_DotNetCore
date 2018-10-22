d18a881ffc444e85c92510a166f8d22662c55e56
RogerioSobrinho <rogerioa.sobrinho@gmail.com>
2018-10-22 15:04:51 -0300

    initial

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using API.Models.JWT;
+using API.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public object Post(
+            [FromBody]User usuario,
+            [FromServices] UsersDAO usersDAO,
+            [FromServices] SigningConfigurations signingConfigurations,
+            [FromServices] TokenConfigurations tokenConfigurations)
+        {
+            bool credenciaisValidas = false;
+            if (usuario != null && !String.IsNullOrWhiteSpace(usuario.UserName))
+            {
+                var usuarioBase = usersDAO.Find(usuario.UserName);
+                credenciaisValidas = (usuarioBase != null &&
+                    usuario.UserName == usuarioBase.UserName &&
+                    usuario.Password == usuarioBase.Password);
+            }
+
+            if (credenciaisValidas)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity(usuario.UserName, "Login"),
+                    new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, usuario.UserName)
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao +
+                    TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = tokenConfigurations.Issuer,
+                    Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK"
+                };
+            }
+            else
+            {
+                return new
+                {
+                    authenticated = false,
+                    message = "Falha ao autenticar"
+                };
+            }
+        }
+    }
+}
---
+++ API/Repository/UsersDAO.cs
@@ -0,0 +1,32 @@
+﻿using API.Models.JWT;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repository
+{
+    public class UsersDAO
+    {
+        private IConfiguration _configuration;
+
+        public UsersDAO(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public User Find(string userName)
+        {
+            using (MySqlConnection conexao = new MySqlConnection(_configuration.GetConnectionString("ExemploJWT")))
+            {
+                return conexao.QueryFirstOrDefault<User>(
+                    "SELECT UserName, Password " +
+                    "FROM Usuarios " +
+                    "WHERE UserName = @UserName", new { UserName = userName });
+            }
+        }
+    }
+}
